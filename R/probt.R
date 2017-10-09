724d953ba2dd14ee5ee96485fda768367063205d
Christopher Prener <chris.prener@slu.edu>
2017-10-08 20:39:53 -0500

    add probt() function

---
+++ R/probt.R
@@ -0,0 +1,16 @@
+#' p-value for t-test
+#'
+#' This function returns the p-value for a t-test.
+#'
+#' @useage probt(t, df)
+#'
+#' @param t t value calculated from the given test
+#' @param df Degrees of freedom
+#'
+#' @example
+#' probt(3.6308, dof = 72)
+#'
+#' @export
+probt <- function(t, df) {
+  2*pt(-abs(t), df = df)
+}
