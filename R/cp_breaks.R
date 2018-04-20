e853ff272bb186e8a265f1a2dec453b43e050680
Christopher Prener <chris.prener@slu.edu>
2018-04-19 21:06:18 -0500

    finish breaks function

--- R/cp_breaks.R
+++ R/cp_breaks.R
@@ -1,7 +1,24 @@
+#' Calculate Map Breaks
+#'
+#' This function wraps the process I have been using to construction data classes for
+#' choropleth maps using \code{ggplot2}.
+#'
+#' @param .data A tbl or sf object
+#' @param var The existing variable to create classes of
+#' @param newvar The new variable for storing the factor data
+#' @param classes The number of breaks or classes to create
+#' @param style The method for calculating classes, see classInt::classIntervals
+#'
+#' @importFrom classInt classIntervals
+#'
 #' @export
-breaks <- function(.data, variable, classes, style){
+cp_breaks <- function(.data, var, newvar, classes, style){
 
-breaks <- classInt::classIntervals(.data[[variable]], n = classes, style = style)
-classes <- cut(.data[[variable]], breaks = c(breaks$brks), include.lowest = TRUE)
+  breaks <- classInt::classIntervals(.data[[var]], n = classes, style = style)
+  classes <- cut(.data[[var]], breaks = c(breaks$brks), include.lowest = TRUE)
+
+  .data <- mutate(.data, newvar = classes)
+
+  return(.data)
 
 }
