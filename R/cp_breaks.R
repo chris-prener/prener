8a0221335fb66a3026ee80f3fafc10a88bcae4b8
Christopher Prener <chris.prener@slu.edu>
2018-04-20 19:57:25 -0500

    add quasi-quotation to cp_breaks

--- R/cp_breaks.R
+++ R/cp_breaks.R
@@ -11,15 +11,43 @@
 #'
 #' @importFrom classInt classIntervals
 #' @importFrom dplyr mutate
+#' @importFrom rlang :=
+#' @importFrom rlang enquo
+#' @importFrom rlang quo
+#' @importFrom rlang quo_name
+#' @importFrom rlang sym
 #'
 #' @export
 cp_breaks <- function(.data, var, newvar, classes, style){
 
-  breaks <- classInt::classIntervals(.data[[var]], n = classes, style = style)
-  classes <- cut(.data[[var]], breaks = c(breaks$brks), include.lowest = TRUE)
+  # save parameters to list
+  paramList <- as.list(match.call())
 
-  .data <- dplyr::mutate(.data, newvar = classes)
+  # quote input variables
+  if (!is.character(paramList$var)) {
+    ref <- rlang::enquo(var)
+  } else if (is.character(paramList$var)) {
+    ref <- rlang::quo(!! rlang::sym(var))
+  }
 
+  refQ <- rlang::quo_name(rlang::enquo(ref))
+
+  if (!is.character(paramList$newvar)) {
+    new <- rlang::enquo(newvar)
+  } else if (is.character(paramList$newvar)) {
+    new <- rlang::quo(!! rlang::sym(newvar))
+  }
+
+  newQ <- rlang::quo_name(rlang::enquo(new))
+
+  # calculate breaks and categories
+  breaks <- classInt::classIntervals(.data[[refQ]], n = classes, style = style)
+  categories <- cut(.data[[refQ]], breaks = c(breaks$brks), include.lowest = TRUE)
+
+  # create new variable
+  .data <- dplyr::mutate(.data, !!newQ := categories)
+
+  # return result
   return(.data)
 
 }
